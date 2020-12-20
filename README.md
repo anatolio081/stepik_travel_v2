e5cb4b0f8fb09c38287adad01c317152d9d2c44e
Tereshenko Anatoliy Vitalyevich <anatoliy.tereschenko@rt.ru>
2020-12-20 16:39:47 +0300

    init project

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # stepik_travel_v2
-проектная работа №1 для курса "Flask с нуля на практике"
+
+
+## Installation
+
+### Установка проекта
+Для начала работы с проектом достаточно: 
+- установить Python3 (https://www.python.org/) 
+- клонировать проект в свою рабочую папку
+
+```sh
+git clone https://github.com/anatolio081/stepik_travel.git
+cd stepik_travel
+``` 
+- настроить виртуальное окружение (venv)
+
+```sh
+python3 -m venv env
+source env/bin/activate 
+pip install -U pip
+pip install -r requirements.txt 
+```
+- Запустить проект:
+```sh
+python3 app.py 
+```
---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from data import title, departures, tours
+from data import subtitle, description
+import random
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_index():
+    return render_template('index.html',
+                           title=title,
+                           subtitle=subtitle,
+                           description=description,
+                           tours=tours,
+                           departures=departures)
+
+
+@app.route('/departures/<departure_tag>/')
+def render_depature(departure_tag):
+    tours_set = {}
+    for key, tour in tours.items():
+        if tour["departure"] == departure_tag:
+            tours_set[key] = tour
+    return render_template('departure.html',
+                           title=title,
+                           tours=tours_set,
+                           departure_tag=departure_tag,
+                           departures=departures)
+
+
+@app.route('/tours/<int:id>/')
+def render_tour(id):
+    return render_template('tour.html',
+                           title=title,
+                           id=id,
+                           tours=tours,
+                           departures=departures)
+
+
+app.run()
